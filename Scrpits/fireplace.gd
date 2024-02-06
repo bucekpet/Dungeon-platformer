c64f60de0e0f1f32040f858577032bdbe17aa54f
Jebingo <58402893+Jebingo@users.noreply.github.com>
2024-02-06 14:40:30 +0100

    fireplace tweak

--- Scrpits/fireplace.gd
+++ Scrpits/fireplace.gd
@@ -2,6 +2,12 @@ extends Interactable
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+@export var aflame: bool = false
+
+func _ready() -> void:
+	if aflame:
+		animation_player.play('Active')
+
 
 func _on_interacted(body: Variant) -> void:
 	animation_player.play('Active')
