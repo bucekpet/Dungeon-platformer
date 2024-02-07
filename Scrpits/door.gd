5ed56721afcbeb83d53c6e57c2592e0d5cbb5c16
Jebingo <58402893+Jebingo@users.noreply.github.com>
2024-02-07 17:33:35 +0100

    level tweaks

--- Scrpits/door.gd
+++ Scrpits/door.gd
@@ -1,10 +1,6 @@
-extends CSGCylinder3D
+extends Node3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_lever_interacted(body: Variant) -> void:
-	animation_player.play('Open')
-
-
-func _on_lever_2_interacted(body: Variant) -> void:
 	animation_player.play('Open')
